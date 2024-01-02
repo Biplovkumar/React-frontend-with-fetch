5a60c5f56dce47b3b5507e0be484564860dce3fb
Biplov Kumar <Kumarb@thepaycocld.onmicrosoft.com>
2024-01-02 13:48:59 +0530

    Done without Token

---
+++ src/components/Nav.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {
+    Link, useNavigate
+} from 'react-router-dom';
+
+const Nav = () => {
+
+    const auth = localStorage.getItem('user');
+    const navigate = useNavigate();
+    const logout = () => {
+        localStorage.clear();
+        navigate('/signup')
+    }
+
+
+let name;
+try {
+    name = auth ? JSON.parse(auth).name : null;
+} catch (error) {
+  console.error("Error parsing JSON:", error);
+}
+
+
+    return (
+        <div>
+            <img
+            alt='logo'
+            className='logo'
+             src='https://biplovkumar.github.io/img/profile.jpg' />
+            {
+                auth ?
+                    <ul className="nav-ul">
+                        <li><Link to="/">Products</Link></li>
+                        <li><Link to="/add">Add Products</Link></li>
+                        <li><Link to="/update"> Update Products</Link></li>
+                        <li><Link to="/profile">Profile</Link></li>
+                        <li><Link onClick={logout} to="/signup">Logout ({name})</Link></li>
+                    </ul>
+                    :
+                    <ul className="nav-ul nav-right">
+                        <li> <Link to="/signup">Sign Up</Link></li>
+                        <li><Link to="/login">Login</Link></li>
+                    </ul>
+            }
+
+
+        </div>
+    )
+}
+
+export default Nav;
