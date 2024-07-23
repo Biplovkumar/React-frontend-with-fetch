8072b6bfa077780c82c35e8bf2d93fcd20d049b9
Biplov Kumar <Kumarb@thepaycocld.onmicrosoft.com>
2024-07-23 10:37:27 +0530

    Added some data

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,7 @@
 const URL = 'http://localhost:4000/'
 const image = URL + 'image/'
-const dummy = 'https://biplovkumar.github.io/img/profile.jpg'
+// const dummy = 'https://biplovkumar.github.io/img/profile.jpg'
+const dummy = 'https://developerasmita1998.github.io/img/profile.jpg'
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default { URL, image, dummy }
